4d726aaa93d6e735ea563b00140cbbeaf48d63f8
Alec Roessler <alec_roessler1@baylor.edu>
2025-08-08 09:34:16 -0500

    s

---
+++ parallel/2_device_convergence_check/kernel.cu
@@ -0,0 +1,90 @@
+// Kernel algorithm
+__global__ void compute_temperature(double* T, double* T_new, double* q, double k, 
+    int grid_size, double h, double T_amb) {
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (x >= grid_size || y >= grid_size) return;
+
+    int idx = y * grid_size + x;
+
+    // Apply Dirichlet boundary conditions
+    if (x == 0 || x == grid_size - 1 || y == 0 || y == grid_size - 1) {
+        T_new[idx] = T_amb;
+        return;
+    }
+
+    // Compute 1D indices for neighbors
+    int top    = (y - 1) * grid_size + x;
+    int bottom = (y + 1) * grid_size + x;
+    int left   = y * grid_size + (x - 1);
+    int right  = y * grid_size + (x + 1);
+
+    double coeff = (h * h / k) * q[idx];
+
+    T_new[idx] = (T[top] + T[bottom] + T[left] + T[right] + coeff) / 4.0;
+}
+
+__global__ max_diff_reduction(double* T, double* T_new, double* max_diff, int total_size) {
+    __shared__ double data[256];
+    int local_index = threadIdx.y * blockDim.x + threadIdx.x;
+    int global_index = blockIdx.x * blockDim.x * blockDim.y + local_index;
+
+
+    // Compute difference for each thread
+    double difference = 0.0;
+    if (global_index < total_size) {
+        difference = fabs(T_new[global_index] - T[global_index]);
+    }
+
+    data[local_index] = difference;
+    __syncthreads();
+
+    // Max reduction
+    for (int stride = 128; stride > 0; stride /= 2) {
+        if (local_index  < stride) {
+            data[local_index] = fmax(data[local_index], data[local_index + stride]);
+        }
+        __syncthreads();
+    }
+
+    // Return the maximum difference at index 0
+    if (local_index  == 0) {
+        max_diff[blockIdx.x] = data[0];
+    }
+}
+
+// Compute the maximum absolute difference between two arrays
+double max_abs_diff(double* a, double* b, int size) {
+    double max_diff = 0.0;
+    for (int i = 0; i < size; ++i) {
+        double diff = fabs(a[i] - b[i]);
+        if (diff > max_diff) max_diff = diff;
+    }
+    return max_diff;
+}
+
+
+// Compute the maximum, minimum, and average temperature in the grid
+double max_temp(double* arr, int grid_size) {
+    double max_val = arr[0];
+    for (int i = 0; i < grid_size * grid_size; i++) {
+        if (arr[i] > max_val) max_val = arr[i];
+    }
+    return max_val;  
+}
+double min_temp(double* arr, int grid_size) {
+    double min_val = arr[0];
+    for (int i = 0; i < grid_size * grid_size; i++) {
+        if (arr[i] < min_val) min_val = arr[i];
+    }
+    return min_val;
+}
+double avg_temp(double* arr, int grid_size) {
+    double sum = 0.0;
+    for (int i = 0; i < grid_size * grid_size; i++) {
+        sum += arr[i];
+    }
+    return (sum / (grid_size * grid_size));
+}
+
---
+++ parallel/2_device_convergence_check/main.cu
@@ -0,0 +1,200 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "support.h"
+#include "kernel.cu"
+
+// Set up parameters
+const int GRID_SIZE = 256;
+const int total_size = GRID_SIZE * GRID_SIZE;
+const char* POWER_MAP_FILE = "/home/roesslera/code/MPP_project_temp_simulation/data/power_map_256.csv";
+const double T_amb = 25;  // Ambient temperature in Celcius
+const int ITERATIONS = 50000;
+const double DIE_WIDTH_M = 0.016;
+const double h = DIE_WIDTH_M / GRID_SIZE;  
+const double k = 150.0; // thermal conductivity (using silicon)
+
+int load_power_map(const char* filename, double* q) {
+    // Confirm file opens
+    FILE* file = fopen(filename, "r");
+    if (!file) {
+        fprintf(stderr, "Error opening file:\n");
+        return 1;
+    }
+
+    // Read file data
+    for (int i = 0; i < total_size; i++) {
+        fscanf(file, "%lf,", &q[i]);
+    }
+
+    fclose(file);
+    return 0;
+}
+
+
+int main(int argc, char* argv[])
+{
+    Timer timer, total_timer, timer_copy, timer_max_device, timer_max_host, timer_kernel;
+    float t_copy = 0, t_max = 0, t_kernel = 0, t_max_host = 0, t_max_device = 0;
+    startTime(&total_timer);
+    cudaError_t cuda_ret;
+
+    // Initialize host variables ----------------------------------------------
+
+    printf("\nSetting up the problem..."); fflush(stdout);
+    startTime(&timer);
+
+    double *q_h, *T_h, *T_new_h, *max_diff_h;
+
+    q_h = (double*) malloc( sizeof(double) * total_size );
+    T_h = (double*) malloc( sizeof(double) * total_size );
+    T_new_h = (double*) malloc( sizeof(double) * total_size );
+    max_diff_h = (double*) malloc(sizeof(double) * blocks);
+
+
+    for (unsigned int i=0; i < total_size; i++) { T_new_h[i] = T_amb; T_h[i] = T_amb; }
+    if (load_power_map(POWER_MAP_FILE, q_h) != 0) {
+        fprintf(stderr, "Failed to load power map.\n");
+        return 1;
+    }
+
+    stopTime(&timer); printf("%f s\n", elapsedTime(timer));
+
+    // Allocate device variables ----------------------------------------------
+
+    printf("Allocating device variables..."); fflush(stdout);
+    startTime(&timer);
+
+    double *q_d, *T_d, *T_new_d, *max_diff_d;
+
+    // CUDA device variables for q, T, and T_new
+    cuda_ret = cudaMalloc((void**)&q_d, sizeof(double)* total_size);
+    if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory for q_d");
+
+    cuda_ret = cudaMalloc((void**)&T_d, sizeof(double)* total_size);
+    if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory for T_d");
+
+    cuda_ret = cudaMalloc((void**)&T_new_d, sizeof(double)* total_size);
+    if(cuda_ret != cudaSuccess) FATAL("Unable to allocate device memory for T_new_d");
+
+    // Reduction kernel function for maximum difference
+    int threads_per_block = 16; // blockDim.x * blockDim.y
+    int blocks = 256; //(GRID_SIZE * GRID_SIZE + threads_per_block - 1) / threads_per_block
+    cudaMalloc(&max_diff_d, blocks * sizeof(double));
+
+
+
+    cudaDeviceSynchronize();
+    stopTime(&timer); printf("%f s\n", elapsedTime(timer));
+
+    // Copy host variables to device ------------------------------------------
+
+    printf("Copying data from host to device..."); fflush(stdout);
+    startTime(&timer);
+
+    // Copy q, T, and T_new from host to device
+    cuda_ret = cudaMemcpy(q_d, q_h, sizeof(double)*total_size, cudaMemcpyHostToDevice);
+    if(cuda_ret != cudaSuccess) FATAL("Unable to copy q from host to device");
+    cuda_ret = cudaMemcpy(T_d, T_h, sizeof(double)*total_size, cudaMemcpyHostToDevice);
+    if(cuda_ret != cudaSuccess) FATAL("Unable to copy T from host to device");
+    cuda_ret = cudaMemcpy(T_new_d, T_new_h, sizeof(double)*total_size, cudaMemcpyHostToDevice);
+    if(cuda_ret != cudaSuccess) FATAL("Unable to copy T_new from host to device");
+
+    cudaDeviceSynchronize();
+    stopTime(&timer); printf("%f s\n", elapsedTime(timer));
+
+    // Launch kernel ---------------------------
+    startTime(&timer);
+    
+    // Define grid and block dimensions
+    dim3 blockDim(16, 16);
+    dim3 gridDim((GRID_SIZE + blockDim.x - 1) / blockDim.x, (GRID_SIZE + blockDim.y - 1) / blockDim.y);
+
+
+    // Launch the kernel
+    int iter;
+    for (iter = 0; iter < ITERATIONS; iter++) {
+        startTime(&timer_kernel);
+        compute_temperature<<<gridDim, blockDim>>>(T_d, T_new_d, q_d, k, GRID_SIZE, h, T_amb);
+        stopTime(&timer_kernel); t_kernel += elapsedTime(timer_kernel);
+        cuda_ret = cudaGetLastError();
+        if(cuda_ret != cudaSuccess) FATAL("Unable to launch kernel");
+
+        // Launch reduction kernel to compute maximum difference
+        startTime(&timer_max_device);
+        max_diff_reduction<<<blocks, blockDim>>>(T_d, T_new_d, max_diff_d, total_size);
+        cudaDeviceSynchronize();
+        stopTime(&timer_max_device); t_max_device += elapsedTime(timer_max_device);
+
+        // Copy max_diff_d from device to host
+        startTime(&timer_copy);
+        cudaMemcpy(max_diff_h, max_diff_d, sizeof(double) * blocks, cudaMemcpyDeviceToHost);
+        stopTime(&timer_copy); t_copy += elapsedTime(timer_copy);
+
+        // Complete reduction on host
+        startTime(&timer_max_host);
+        double max_change = 0.0;
+        for (int i = 0; i < blocks; i++) {
+            if (max_diff_h[i] > max_change) {
+                max_change = max_diff_h[i];
+            }
+        }
+        stopTime(&timer_max_host); t_max_host += elapsedTime(timer_max_host);
+        
+        // Check for convergence
+        if (max_change < 1e-3) {
+            printf("Converged after %d iterations\n", iter);
+            break;
+        }
+        
+        // Swap T and T_new pointers
+        double* temp = T_d;
+        T_d = T_new_d;
+        T_new_d = temp;
+    }
+
+    cuda_ret = cudaDeviceSynchronize();
+
+    // Copy device variables from host ----------------------------------------
+
+    printf("Copying data from device to host..."); fflush(stdout);
+    startTime(&timer);
+
+    cuda_ret = cudaMemcpy(T_new_h, T_new_d, sizeof(double)*total_size, cudaMemcpyDeviceToHost);
+    if(cuda_ret != cudaSuccess) FATAL("Unable to copy memory from device");
+
+    cudaDeviceSynchronize();
+    stopTime(&timer); printf("%f s\n", elapsedTime(timer));
+
+    double max_temp_T = max_temp(T_new_h, GRID_SIZE);
+    double min_temp_T = min_temp(T_new_h, GRID_SIZE);
+    double avg_temp_T = avg_temp(T_new_h, GRID_SIZE);
+
+    printf("Max Temp: %.2f C\n", max_temp_T);
+    printf("Min Temp: %.2f C\n", min_temp_T);
+    printf("Avg Temp: %.2f C\n", avg_temp_T);
+
+    // Verify correctness -----------------------------------------------------
+    
+    printf("Verifying results..."); fflush(stdout);
+    verify(iter, max_temp_T, min_temp_T, avg_temp_T);
+    
+    // Free memory ------------------------------------------------------------
+
+    free(q_h);
+    free(T_h);
+    free(T_new_h);
+
+    // Free device variables
+    cudaFree(q_d);
+    cudaFree(T_d);
+    cudaFree(T_new_d);
+
+    printf("Reduce Temperature kernel time for convergence check: %.5f s\n", t_max_device);
+    printf("Copy reduced Temperature to host for convergence check: %.5f s\n", t_copy);
+    printf("Reduce Temperature further on host for convergence check: %.5f s\n", t_max_host);
+    printf("Kernel algorithm for temperature computation execution time: %.5f s\n", t_kernel);
+
+    stopTime(&total_timer); printf("Total Execution Time: %f s\n", elapsedTime(total_timer));
+
+    return 0;
+}
