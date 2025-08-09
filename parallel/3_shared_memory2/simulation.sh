9bd8cf0fd5bb843c125c70aa5c83154c34af4dfe
Alec Roessler <alec_roessler1@baylor.edu>
2025-08-09 12:10:49 -0500

    s

--- parallel/3_shared_memory2/simulation.sh
+++ parallel/3_shared_memory2/simulation.sh
@@ -1,1 +1,1 @@
-/home/roesslera/code/MPP_project_temp_simulation/parallel/2_device_convergence_check/simulation
+/home/roesslera/code/MPP_project_temp_simulation/parallel/3_shared_memory2/simulation
