1676285e96821f0fe00515e18954a67b11129bc7
Alec Roessler <alec_roessler1@baylor.edu>
2025-08-09 12:09:05 -0500

    s

--- parallel/3_shared_memory2/simulation.sh
+++ parallel/3_shared_memory2/simulation.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-#PBS -N simulation_job
-#PBS -l nodes=1:ppn=1:gpus=1
-#PBS -q instructional
-#PBS -j oe
-#PBS -o simulation.out
-
-# Always change to the directory from which qsub/mpprun was called
-cd "$PBS_O_WORKDIR"
-
-# Optional: load CUDA module if needed by your cluster
-# module load cuda/11.8
-
-# Run the compiled binary
-./simulation
+/home/roesslera/code/MPP_project_temp_simulation/parallel/2_device_convergence_check/simulation
