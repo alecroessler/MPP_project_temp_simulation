e789f71d144ae7403096859c2718fa3b669c9be9
Alec Roessler <alec_roessler1@baylor.edu>
2025-08-11 09:23:22 -0500

    s

--- parallel/3_full_device_convergence_check/kernel.cu
+++ parallel/3_full_device_convergence_check/kernel.cu
@@ -1,5 +1,5 @@
 // Kernel algorithm for temperature computation
-__global__ void compute_temperature(const float* T, float* T_new, const double* q, double k, 
+__global__ void compute_temperature(const double* T, double* T_new, const double* q, double k, 
     int grid_size, double h, double T_amb) {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
@@ -28,7 +28,7 @@ __global__ void compute_temperature(const float* T, float* T_new, const double* 
 }
 
 // Kernel for reduction to find maximum difference
-__global__ void max_diff_reduction(float* T, float* T_new, float* max_diff, int total_size) {
+__global__ void max_diff_reduction(double* T, double* T_new, double* max_diff, int total_size) {
     __shared__ double data[256];
     int local_index = threadIdx.y * blockDim.x + threadIdx.x;
     int global_index = blockIdx.x * blockDim.x * blockDim.y + local_index;
